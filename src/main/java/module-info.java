04aef3ccb442984d2595e569466aec06e07b08ad
Jose Gabriel <joseg.paguayi@uqvirtual.edu.co>
2025-09-26 07:57:37 -0500

    configuraciones principales

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.proyectofinalprog2code {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.proyectofinalprog2code to javafx.fxml;
+    exports co.edu.uniquindio.proyectofinalprog2code;
+}
